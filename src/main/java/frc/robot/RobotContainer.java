4ea1922c3f4d76da058c58809d1a388f1ad2455e
Rorybabory <lambertrory123@gmail.com>
2023-02-18 10:08:47 -0800

    added arm, tweaked swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,11 +8,14 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.DriveCommand;
+import frc.robot.commands.ExtendArm;
 import frc.robot.commands.TurnWrist;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Wrist;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.ButtonConstants;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,13 +27,18 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final DriveTrain drivetrain = new DriveTrain();
   //private final Wrist wrist = new Wrist();
+  private final Arm arm = new Arm();
   private final Joystick joystick;
+  private final JoystickButton b_armExtend;
+  private final JoystickButton b_armRetract;
   //private final JoystickButton b_turnWrist;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     joystick = new Joystick(0);
+    b_armExtend = new JoystickButton(joystick, ButtonConstants.BUTTON_ARM_EXTEND);
+    b_armRetract = new JoystickButton(joystick, ButtonConstants.BUTTON_ARM_RETRACT);
     //b_turnWrist = new JoystickButton(joystick, Constants.ButtonConstants.BUTTON_WRIST);
     configureButtonBindings();
   }
@@ -44,6 +52,8 @@ public class RobotContainer {
   private void configureButtonBindings() {
     //b_turnWrist.onTrue(new TurnWrist(wrist));
     drivetrain.setDefaultCommand(new DriveCommand(drivetrain, joystick));
+    b_armExtend.onTrue(new ExtendArm(arm,0.1));
+    b_armRetract.onTrue(new ExtendArm(arm,-0.1));
   }
 
   /**
