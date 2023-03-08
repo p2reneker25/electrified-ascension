935218976f9b3578ad3e229170d5f1d6bf19665f
GalenSpooner <gspooner@cusd.me>
2023-03-07 16:33:36 -0800

    added pivot code

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,14 +1,24 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import frc.robot.Constants;
+
+
 public class Arm {
+    private TalonFX talon;
     private MotorController armMotor;
     public Arm(){
         armMotor = new VictorSP(Constants.ArmConstants.ARM_PWM);
+        talon = new TalonFX(Constants.ArmConstants.PIVOT_CAN);
     }
     public void setMotor(double speed){
         armMotor.set(speed);
+    }
+    public void setPivot(double speed){
+        talon.set(TalonFXControlMode.PercentOutput, speed);
     }
 
     
