ef272f4da5dc7a383ae113cbf30c7b1e8e6dd99d
Rorybabory <lambertrory123@gmail.com>
2023-05-02 16:52:24 -0700

    documentation

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -29,12 +29,11 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 public class DriveTrain extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
+  
   public final DriveModule frontright;
   public final DriveModule frontleft;
   public final DriveModule backright;
   public final DriveModule backleft;
-  DoubleLogEntry encoderLog;
   public ChassisSpeeds chassisSpeeds;
 
   public boolean autoMode = false;
@@ -56,6 +55,8 @@ public class DriveTrain extends SubsystemBase {
   );
   SwerveDriveOdometry m_odometry;
   public DriveTrain() {
+
+    //initializing each module based on constants, explained in DriveModule
     frontright = new DriveModule(
       DriveConstants.FRONTRIGHT_MODULE_DRIVE_CAN,
       DriveConstants.FRONTRIGHT_MODULE_STEER_CAN,
@@ -80,68 +81,63 @@ public class DriveTrain extends SubsystemBase {
       DriveConstants.BACKLEFT_MODULE_ENCODER,
       DriveConstants.BACKLEFT_MODULE_OFFSET
     );
+
+    //initializing odometry
     m_odometry = new SwerveDriveOdometry(kinematics, new Rotation2d(), 
-    
-    new SwerveModulePosition[] {
-      new SwerveModulePosition(frontright.getDriveEncoder(), new Rotation2d()),
-      new SwerveModulePosition(frontleft.getDriveEncoder(), new Rotation2d()),
-      new SwerveModulePosition(backright.getDriveEncoder(), new Rotation2d()),
-      new SwerveModulePosition(backleft.getDriveEncoder(), new Rotation2d())
-    }, new Pose2d(0,0, new Rotation2d())
-  );
-      chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
-      SmartDashboard.putNumber("P", 0.008);
-      SmartDashboard.putNumber("D", 0.0);
-      NAVX.reset();
-      // navxpid = new PIDController(0.025, 0, 0);
-      navxpid = new PIDController(0.005, 0, 0);
+      new SwerveModulePosition[] {
+        new SwerveModulePosition(frontright.getDriveEncoder(), new Rotation2d()),
+        new SwerveModulePosition(frontleft.getDriveEncoder(), new Rotation2d()),
+        new SwerveModulePosition(backright.getDriveEncoder(), new Rotation2d()),
+        new SwerveModulePosition(backleft.getDriveEncoder(), new Rotation2d())
+      }, new Pose2d(0,0, new Rotation2d())
+    );
+    chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+    SmartDashboard.putNumber("P", 0.008);
+    SmartDashboard.putNumber("D", 0.0);
+    NAVX.reset();
+    // navxpid = new PIDController(0.025, 0, 0);
+    navxpid = new PIDController(0.005, 0, 0);
 
-      DataLogManager.start();
-      DriverStation.startDataLog(DataLogManager.getLog());
-      DataLog log = DataLogManager.getLog();
-      encoderLog = new DoubleLogEntry(log, "encoder values");
-      NAVX.setAngleAdjustment(180);
+    DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
+    DataLog log = DataLogManager.getLog();
 
-    }
-  public void resetFOD() {
+    //flips the navx, as the robot begins the match facing the driver
+    NAVX.setAngleAdjustment(180);
+
+  }
+  public void resetFOD() { //reset Field Oriented Drive, ie resets the NAVX
     NAVX.setAngleAdjustment(0);
     NAVX.reset();
   }
   public void drive(double x, double y, double z, int hat) {
     if (autoMode) {return;}
-    double addedsteer = 0;
 
     if (Math.abs(x) < 0.05) {x = 0;}
     if (Math.abs(y) < 0.05) {y = 0;}
-    if (Math.abs(z) < 0.05) {
-      z = 0;
-      addedsteer = getNAVXStraightenVal();
-    }else {
-      navxoffset = NAVX.getAngle();
-    }
+    if (Math.abs(z) < 0.05) {z = 0;}
     drive_magnitude = Math.sqrt(x*x+y*y+z*z);
-    // chassisSpeeds.vxMetersPerSecond = y;
-    // chassisSpeeds.vyMetersPerSecond = x;
-    chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(-y, -x, z, NAVX.getRotation2d());
-    // chassisSpeeds = new ChassisSpeeds(y, x, z);
-    hatValue = hat;
-    
-    // chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(chassisSpeeds, NAVX.getRotation2d());
+
+    //chassisSpeeds stores the ideal translation and rotation of the robot.
+    chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(-y, -x, z, NAVX.getRotation2d()); //Use this for field oriented
+    // chassisSpeeds = new ChassisSpeeds(y, x, z); //use this for non field oriented
+
+    hatValue = hat; //stores hatvalue for future use
   }
-  public void driveAuto(double x, double y, double z) {
+  public void driveAuto(double x, double y, double z) { //drive command for auto with no deadband and no FOD
     chassisSpeeds = new ChassisSpeeds(y, x, z);
   }
-  public void driveInDirection(double speed, double angle) {
+  public void driveInDirection(double speed, double angle) { //drives in a specific direction by manually setting the module values
     frontright.set(speed, 0, true);
     frontleft.set(speed, 180, false);
     backright.set(speed, 180, false);
     backleft.set(speed, 0, true);
 
   }
-  public void setPitchOffset(double p) {
+  public void setPitchOffset(double p) { //Auto platform stuff, pls ignore
     pitchoffset = p;
   }
-  public double clampToAngle(double i) {
+  public double clampToAngle(double i) { //helper method to clamp angle to 0 to 360
     if (i > 360.0) {
       i-=360.0;
     }else if (i < 0.0) {
@@ -149,129 +145,94 @@ public class DriveTrain extends SubsystemBase {
     }
     return i;
   }
-  public double getContinousDist(double a1, double a2) {
-    double error1 = (a1-a2+360.0);
-    double error2 = (a1-a2);
-
-    
-
-    if (Math.abs(error1) < Math.abs(error2)) {
-        return (error1);
-    }else {
-        return (error2);
-    }
-    
-  }
-  public boolean shouldSwap(double encoder, double state) {
+  public boolean shouldSwap(double encoder, double state) { //returns whether to optimize the swerve drive by finding the error between the state and encoder values
     return 180-Math.abs(Math.abs(state-encoder)-180) > 90;
   }
+
+  //swerve optimize switches the direction of the motor when the steer has to rotate more then 90 degrees, based off the currentangle and swerve state
   public SwerveModuleState optimize(
       SwerveModuleState desiredState, double currentAngle) {
-    boolean swap = shouldSwap(stateToOptimize(desiredState.angle.getDegrees()), currentAngle);
+    //runs shouldSwap, and uses stateToOptimize to put them both in the same frame of reference
+    boolean swap = shouldSwap(stateToOptimize(desiredState.angle.getDegrees()), currentAngle); 
     if (swap) {
+      //returns a flipped swerve state
       return new SwerveModuleState(
         -desiredState.speedMetersPerSecond,
         desiredState.angle.rotateBy(Rotation2d.fromDegrees(180.0)));
     }else {
+      //returns the original swerve state
       return new SwerveModuleState(desiredState.speedMetersPerSecond, desiredState.angle);
     }
   }
-  public Translation3d getAccelerometer() {
+  public Translation3d getAccelerometer() { //gets the accelerometer values, pretty self explanatory
     return new Translation3d(accelerometer.getX(), accelerometer.getY(), accelerometer.getZ());
   }
-  public double encoderToOptimize(double encval, float offset) {
-    // encval = encval % 360;
+  public double encoderToOptimize(double encval, float offset) { //puts encoder value into proper frame of reference
     encval -= offset;
     if (encval > 360) {encval-=360;}
     if (encval < 0) {encval+=360;}
-    // encval -= 270;
-    // if (encval > 360) {encval-=360;}
-    // if (encval < 0) {encval+=360;}
     return encval;
   }
-  public double stateToOptimize(double stateval) {
+  public double stateToOptimize(double stateval) { //flips state value
     // if (stateval > 360) {stateval-=360;}
     // if (stateval < 0) {stateval+=360;}
     stateval += 180;
     return stateval;
   }
-  public double getNAVXStraightenVal() {
+  public double getNAVXStraightenVal() { //can use a PID loop to calculate an angle to rotate the robot to avoid drift. Tested but not currently used
     return navxpid.calculate(NAVX.getAngle()-navxoffset, 0);
   }
-  public double getDriveEncoderAvg() {
+  public double getDriveEncoderAvg() { //averages all the drive encoders for more convenient auto programming
     return ((frontright.getDriveEncoder() + frontleft.getDriveEncoder() +
             backright.getDriveEncoder() + backleft.getDriveEncoder()/4.0));
   }
-  public double toMagnitude(double x) {
-    if (x < 0) {
-      x = -drive_magnitude;
-    }else {
-      x = drive_magnitude;
-    }
-    return x;
-  }
-  public double getPitch() {
+  public double getPitch() { //Gets pitch of the robot for autonomous platform
     return NAVX.getPitch()+pitchoffset+10;
   }
   @Override
   public void periodic() {
-    
+    //takes the kinematics with the module positions and the chassisSpeeds from the drive to give the desired states for each module
+    //A swerve module state contains the desired speed and angle for a module
     SwerveModuleState[] state = kinematics.toSwerveModuleStates(chassisSpeeds);
-    SmartDashboard.putNumber("Average Encoder", frontright.getDriveEncoder()/Constants.DriveConstants.ENCODER_PER_INCH);
-    // SmartDashboard.putNumber("Optimize Encoder", encoderToOptimize(frontleft.getEncoder(), 64));
-    // SmartDashboard.putNumber("Optimize State", stateToOptimize(state[3].angle.getDegrees()));
-    // SmartDashboard.putNumber("NAVX PITCH", NAVX.getPitch());
-    
-    // System.out.println("shouldSwap: " + shouldSwap(encoderToOptimize(frontleft.getEncoder(), 64), stateToOptimize(state[3].angle.getDegrees())));
-    // state[0] = SwerveModuleState.optimize(state[0], new Rotation2d(backright.getNEOEncoder() * (3.14/180.0)));
-    // // state[1] = SwerveModuleState.optimize(state[1], new Rotation2d(backleft.getNEOEncoder() * (3.14/180.0)));
+
+    //runs optimize code, explained above. TODO: put offsets into constants
     state[0] = optimize(state[0], encoderToOptimize(backright.getEncoder(), 248));    
     state[1] = optimize(state[1], encoderToOptimize(backleft.getEncoder(), 108));    
-
     state[2] = optimize(state[2], encoderToOptimize(frontright.getEncoder(), 97));    
     state[3] = optimize(state[3], encoderToOptimize(frontleft.getEncoder(), 64));    
-    // System.out.println("encoder: " + encoderToOptimize(encval) + " state angle: " + stateToOptimize(stateval));
-    // state[3] = SwerveModuleState.optimize(state[3], new Rotation2d(-frontleft.getNEOEncoder() * (3.14/180.0)));
-    // if (Math.abs(chassisSpeeds.vxMetersPerSecond) < 0.01 &&
-    //     Math.abs(chassisSpeeds.vyMetersPerSecond) < 0.01 && 
-    //     Math.abs(chassisSpeeds.omegaRadiansPerSecond) < 0.01) {
-    //       frontright.stop();
-    //       frontleft.stop();
-    //       backright.stop();
-    //       backleft.stop();
-    //       return;
-          
-    //     }
+
     SmartDashboard.putNumber("PITCH", getPitch());
     if (autoMode == true) {
       return;
     }
-    double avg = (frontright.getDriveEncoder()+frontleft.getDriveEncoder()+backright.getDriveEncoder()+backleft.getDriveEncoder())/4.0;
-    SmartDashboard.putNumber("frontright speed: ", Math.abs(state[0].speedMetersPerSecond));
+    
+    //A WHOLE BUNCH OF SMART DASHBOARD DEBUGGING VALUES. Commented out for shuffleboard performance
+    
+    // SmartDashboard.putNumber("frontright speed: ", Math.abs(state[0].speedMetersPerSecond));
     // // SmartDashboard.putNumber("frontright angle: ", state[0].angle.getDegrees());
-    SmartDashboard.putNumber("frontright encoder: ", frontright.getEncoder());
-    SmartDashboard.putNumber("frontright offset: ", Constants.DriveConstants.FRONTRIGHT_MODULE_OFFSET*360.0);
+    // SmartDashboard.putNumber("frontright encoder: ", frontright.getEncoder());
+    // SmartDashboard.putNumber("frontright offset: ", Constants.DriveConstants.FRONTRIGHT_MODULE_OFFSET*360.0);
     // SmartDashboard.putNumber("frontright deviation: ", Math.abs(frontright.drive.getEncoder().getVelocity()));
     // // SmartDashboard.putNumber("frontright current", frontright.drive.getOutputCurrent());
 
     // SmartDashboard.putNumber("frontleft speed: ", Math.abs(state[1].speedMetersPerSecond));
     // // SmartDashboard.putNumber("frontleft angle: ", state[1].angle.getDegrees());
-    SmartDashboard.putNumber("frontleft encoder: ", frontleft.getEncoder());
-    SmartDashboard.putNumber("frontleft offset: ", Constants.DriveConstants.FRONTLEFT_MODULE_OFFSET*360.0);
+    // SmartDashboard.putNumber("frontleft encoder: ", frontleft.getEncoder());
+    // SmartDashboard.putNumber("frontleft offset: ", Constants.DriveConstants.FRONTLEFT_MODULE_OFFSET*360.0);
     // SmartDashboard.putNumber("frontleft deviation: ", Math.abs(frontleft.drive.getEncoder().getVelocity()));
     // // SmartDashboard.putNumber("frontleft current", frontleft.drive.getOutputCurrent());
 
     // SmartDashboard.putNumber("backright speed: ", Math.abs(state[2].speedMetersPerSecond));
     // // SmartDashboard.putNumber("backright angle: ", state[2].angle.getDegrees());
-    SmartDashboard.putNumber("backright encoder: ", backright.getEncoder());
-    SmartDashboard.putNumber("backright offset: ", Constants.DriveConstants.BACKRIGHT_MODULE_OFFSET*360.0);
+    // SmartDashboard.putNumber("backright encoder: ", backright.getEncoder());
+    // SmartDashboard.putNumber("backright offset: ", Constants.DriveConstants.BACKRIGHT_MODULE_OFFSET*360.0);
     // SmartDashboard.putNumber("backright deviation: ", Math.abs(backright.drive.getEncoder().getVelocity()));
     // // SmartDashboard.putNumber("backright current", backright.drive.getOutputCurrent());
 
     // SmartDashboard.putNumber("backleft speed: ", Math.abs(state[3].speedMetersPerSecond));
     // // SmartDashboard.putNumber("backleft angle: ", state[3].angle.getDegrees());
-    SmartDashboard.putNumber("backleft encoder: ", backleft.getEncoder());
-    SmartDashboard.putNumber("backleft offset: ", Constants.DriveConstants.BACKLEFT_MODULE_OFFSET*360.0);
+    // SmartDashboard.putNumber("backleft encoder: ", backleft.getEncoder());
+    // SmartDashboard.putNumber("backleft offset: ", Constants.DriveConstants.BACKLEFT_MODULE_OFFSET*360.0);
     // SmartDashboard.putNumber("backleft deviation: ", Math.abs(backleft.drive.getEncoder().getVelocity()));
     // // SmartDashboard.putNumber("backleft current", backleft.drive.getOutputCurrent());
 
@@ -283,18 +244,20 @@ public class DriveTrain extends SubsystemBase {
     // if (autoMode) {
     //   return;
     // }
-    if (hatValue != -1) {
+    if (hatValue != -1) { //Allow Creeping the drive with the HAT switch, untested in current branch
       frontright.set(-Constants.DriveConstants.CREEP_SPEED, hatValue, true);
       frontleft.set(Constants.DriveConstants.CREEP_SPEED, hatValue, false);
       backright.set(-Constants.DriveConstants.CREEP_SPEED, hatValue, false);
       backleft.set(-Constants.DriveConstants.CREEP_SPEED, hatValue, true);
     }else {
+      //runs the set function for each individual module. Multiplied by max speed, with certain ones flipped.
       frontright.set(state[0].speedMetersPerSecond * Constants.DriveConstants.MAX_SPEED, state[0].angle.getDegrees(), true);
       frontleft.set(state[1].speedMetersPerSecond * Constants.DriveConstants.MAX_SPEED, state[1].angle.getDegrees()+180, false);
       backright.set(state[2].speedMetersPerSecond * Constants.DriveConstants.MAX_SPEED, state[2].angle.getDegrees()+180, false);
       backleft.set(state[3].speedMetersPerSecond * Constants.DriveConstants.MAX_SPEED, state[3].angle.getDegrees(), true);
     }
     
+    //gets the swerve drive pose for odometry, measurements untested
     Pose2d pose = m_odometry.update(new Rotation2d(), 
     new SwerveModulePosition[] {
       new SwerveModulePosition(frontright.getDriveEncoder()*0.1156, new Rotation2d(frontright.getEncoder() * (3.14/180.0))),
@@ -303,20 +266,11 @@ public class DriveTrain extends SubsystemBase {
       new SwerveModulePosition(backleft.getDriveEncoder()*0.1156, new Rotation2d(backleft.getEncoder() * (3.14/180.0)))
     }
     );
+
+    //debug stuff
     SmartDashboard.putNumber("HAT VALUE", hatValue);
     SmartDashboard.putNumber("odometry x", pose.getX());
     SmartDashboard.putNumber("odometry y", pose.getY());
-    // double p = SmartDashboard.getNumber("P", 0.008);
-    // frontright.pid.setP(p);
-    // frontleft.pid.setP(p);
-    // backright.pid.setP(p);
-    // backleft.pid.setP(p);
-
-    // double d = SmartDashboard.getNumber("D", 0.00);
-    // frontright.pid.setD(d);
-    // frontleft.pid.setD(d);
-    // backright.pid.setD(d);
-    // backleft.pid.setD(d);
     System.out.println("RUNNING PERIODIC: " + autoMode);
   }
 
