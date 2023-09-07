8f4c67b00480432ade4d7c9b55a45d33f9c51f02
p2reneker25 <rreneker@cusd.me>
2023-09-07 14:17:48 -0700

    Added subsystem

---
+++ src/main/java/frc/robot/subsystems/DriveTrainII.java
@@ -0,0 +1,160 @@
+package frc.robot.subsystems;
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveController;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveControllerConfiguration;
+import swervelib.parser.SwerveDriveConfiguration;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
+public class DriveTrainII extends SubsystemBase{
+    private SwerveDrive swerveDrive;
+    private SwerveAutoBuilder autoSwerve;
+    public DriveTrainII(File directory)
+  {
+    // Configure the Telemetry before creating the SwerveDrive to avoid unnecessary objects being created.
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    try
+    {
+      swerveDrive = new SwerveParser(directory).createSwerveDrive();
+    } catch (Exception e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
+  public DriveTrainII(SwerveDriveConfiguration driveCfg, SwerveControllerConfiguration controllerCfg)
+  {
+    swerveDrive = new SwerveDrive(driveCfg, controllerCfg);
+  }
+  public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop)
+  {
+    swerveDrive.drive(translation, rotation, fieldRelative, isOpenLoop);
+  }
+  public SwerveDriveKinematics getKinematics()
+  {
+    return swerveDrive.kinematics;
+  }
+  public void resetOdometry(Pose2d initialHolonomicPose)
+  {
+    swerveDrive.resetOdometry(initialHolonomicPose);
+  }
+  public Pose2d getPose()
+  {
+    return swerveDrive.getPose();
+  }
+  public void setChassisSpeeds(ChassisSpeeds chassisSpeeds)
+  {
+    swerveDrive.setChassisSpeeds(chassisSpeeds);
+  }
+  public void postTrajectory(Trajectory trajectory)
+  {
+    swerveDrive.postTrajectory(trajectory);
+  }
+
+  public void zeroGyro()
+  {
+    swerveDrive.zeroGyro();
+  }
+
+
+  public void setMotorBrake(boolean brake)
+  {
+    swerveDrive.setMotorIdleMode(brake);
+  }
+
+  public Rotation2d getHeading()
+  {
+    return swerveDrive.getYaw();
+  }
+
+  public ChassisSpeeds getTargetSpeeds(double xInput, double yInput, double headingX, double headingY)
+  {
+    xInput = Math.pow(xInput, 3);
+    yInput = Math.pow(yInput, 3);
+    return swerveDrive.swerveController.getTargetSpeeds(xInput, yInput, headingX, headingY, getHeading().getRadians());
+  }
+
+  public ChassisSpeeds getTargetSpeeds(double xInput, double yInput, Rotation2d angle)
+  {
+    xInput = Math.pow(xInput, 3);
+    yInput = Math.pow(yInput, 3);
+    return swerveDrive.swerveController.getTargetSpeeds(xInput, yInput, angle.getRadians(), getHeading().getRadians());
+  }
+
+  public ChassisSpeeds getFieldVelocity()
+  {
+    return swerveDrive.getFieldVelocity();
+  }
+
+  public ChassisSpeeds getRobotVelocity()
+  {
+    return swerveDrive.getRobotVelocity();
+  }
+
+  public SwerveController getSwerveController()
+  {
+    return swerveDrive.swerveController;
+  }
+
+  public SwerveDriveConfiguration getSwerveDriveConfiguration()
+  {
+    return swerveDrive.swerveDriveConfiguration;
+  }
+
+  public void lock()
+  {
+    swerveDrive.lockPose();
+  }
+
+  public Rotation2d getPitch()
+  {
+    return swerveDrive.getPitch();
+  }
+
+  public Command creatPathPlannerCommand(String path, PathConstraints constraints, Map<String, Command> eventMap,
+  PIDConstants translation, PIDConstants rotation, boolean useAllianceColor)
+{
+List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(path, constraints);
+//    SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+//      Pose2d supplier,
+//      Pose2d consumer- used to reset odometry at the beginning of auto,
+//      PID constants to correct for translation error (used to create the X and Y PID controllers),
+//      PID constants to correct for rotation error (used to create the rotation controller),
+//      Module states consumer used to output to the drive subsystem,
+//      Should the path be automatically mirrored depending on alliance color. Optional- defaults to true
+//   )
+if (autoBuilder == null)
+{
+autoBuilder = new SwerveAutoBuilder(
+swerveDrive::getPose,
+swerveDrive::resetOdometry,
+translation,
+rotation,
+swerveDrive::setChassisSpeeds,
+eventMap,
+useAllianceColor,
+this
+);
+}
+
+return autoBuilder.fullAuto(pathGroup);
+}
+}
